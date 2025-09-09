68c6f87f302a5a6a5b997593030f5b396af458a3
Stepoik <83645255+Stepoik@users.noreply.github.com>
2025-09-09 18:48:27 +0300

    added 1 var

--- src/main/java/prac3/Task2_1.java
+++ src/main/java/prac3/Task2_1.java
@@ -3,32 +3,34 @@ package prac3;
 import io.reactivex.rxjava3.core.Observable;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
-public class Task2_2 {
+public class Task2_1 {
     public static void main(String[] args) {
         third();
     }
 
     private static void first() {
         Observable.range(0, 1000)
-                .map((value) ->  new Random().nextInt(0, 1000))
-                .filter((value) -> value > 500)
+                .map((value) -> new Random().nextInt(0, 1000))
+                .map((value) -> value * value)
                 .blockingSubscribe(System.out::println);
     }
 
     private static void second() {
-        var flow1 = Observable.range(0, 1000).map((value) -> "From flow 1 " + value)
-                .delay(1000, TimeUnit.MILLISECONDS);
-        var flow2 = Observable.range(0, 1000).map((value) -> "From flow 2 " + value);
+        var flow1 = Observable.range(0, 1000).map((value) -> {
+                    var random = new Random();
+                    var index = random.nextInt(0, 26);
+                    return Character.toString('A' + index);
+                });
+        var flow2 = Observable.range(0, 1000).map((value) -> new Random().nextInt(0, 10));
 
-        Observable.concat(flow1, flow2)
+        Observable.zip(flow1, flow2, (f1, f2) -> f1 + f2)
                 .blockingSubscribe(System.out::println);
     }
 
     private static void third() {
         Observable.range(0, 10).map((value) -> new Random().nextInt(0, 1000))
-                .take(5)
+                .skip(3)
                 .blockingSubscribe(System.out::println);
     }
 }
